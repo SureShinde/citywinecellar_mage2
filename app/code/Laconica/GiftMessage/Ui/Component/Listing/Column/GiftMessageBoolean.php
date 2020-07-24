3e8243a4e641dcd78dac926d2e45d47d026e3aa7
Holubiatnikova Anna <holubiatnikova.anna@gmail.com>
2020-07-24 13:32:13 +0300

    Gift message function

---
+++ app/code/Laconica/GiftMessage/Ui/Component/Listing/Column/GiftMessageBoolean.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Laconica\GiftMessage\Ui\Component\Listing\Column;
+use \Magento\Sales\Api\OrderRepositoryInterface;
+use \Magento\Framework\View\Element\UiComponent\ContextInterface;
+use \Magento\Framework\View\Element\UiComponentFactory;
+use \Magento\Ui\Component\Listing\Columns\Column;
+use \Magento\Framework\Api\SearchCriteriaBuilder;
+
+class GiftMessageBoolean extends Column
+{
+    /**
+     * @var OrderRepositoryInterface
+     */
+    protected $_orderRepository;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    protected $_searchCriteria;
+
+    /**
+     * GiftId constructor.
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param OrderRepositoryInterface $orderRepository
+     * @param SearchCriteriaBuilder $criteria
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        OrderRepositoryInterface $orderRepository,
+        SearchCriteriaBuilder $criteria,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->_orderRepository = $orderRepository;
+        $this->_searchCriteria  = $criteria;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                $order = $this->_orderRepository->get($item["entity_id"]);
+                $status = $order->getData("gift_message_id");
+                if($status) {
+                    $message = 'Yes';
+                } else{
+                    $message = 'No';
+                }
+                $item[$this->getData('name')] = $message;
+            }
+        }
+        return $dataSource;
+    }
+}
