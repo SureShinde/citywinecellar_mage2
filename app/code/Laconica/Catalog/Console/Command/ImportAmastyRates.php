09b7f3d2f0d7f7fc3315839736ba6e8f7e521bcc
Holubiatnikova Anna <holubiatnikova.anna@gmail.com>
2020-07-31 14:20:57 +0300

    Configure Amasty Shipping Module console command

--- app/code/Laconica/Catalog/Console/Command/ImportAmastyRates.php
+++ app/code/Laconica/Catalog/Console/Command/ImportAmastyRates.php
@@ -44,23 +44,13 @@ class ImportAmastyRates extends Command
 
     public function __construct(
         Files $files,
-        File $file,
         DirectoryList $directoryList,
-        \Magento\Catalog\Model\ImageUploader $imageUploader,
-        PageFactory $pageFactory,
-        LoggerInterface $logger,
-        CategoryFactory $categoryFactory,
         Data $fileHelper,
         \Magento\Framework\App\Response\Http\FileFactory $fileFactory,
         \Magento\Framework\Filesystem $filesystem
     ) {
         $this->_files = $files;
-        $this->file = $file;
         $this->directoryList = $directoryList;
-        $this->imageUploader = $imageUploader;
-        $this->pageFactory = $pageFactory;
-        $this->logger = $logger;
-        $this->categoryFactory = $categoryFactory;
         $this->fileHelper = $fileHelper;
         $this->fileFactory = $fileFactory;
         $this->filesystem = $filesystem;
@@ -88,20 +78,21 @@ class ImportAmastyRates extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $file = $this->_files->getModuleFile('Laconica', 'Import', 'data/TLS.csv');
+        $file = '/var/www/m2.citywinecellar.com/var/import/rates_1.csv';
         $data = $this->fileHelper->getDataFromFile($file);
         $newData = [];
 
         foreach ($data as $key => $item) {
-            $zipCodes = explode(',', $item['zip_from']);
+            $zipCodes = explode(',', $item['Zip from']);
             if (count($zipCodes) > 1) {
                 foreach ($zipCodes as $zipCode) {
                     $newItem = $item;
                     $zipFromTo = explode('-', $zipCode);
                     $zipFrom = $zipFromTo[0];
                     $zipTo = isset($zipFromTo[1]) ? $zipFromTo[1] : $zipFromTo[0];
-                    $newItem['zip_from'] = "$zipFrom";
-                    $newItem['zip_to'] = "$zipTo";
+                    $newItem['Zip from'] = "$zipFrom";
+                    $newItem['Zip to'] = "$zipTo";
+                    $newItem['method_id'] = 3;
                     $newData[] = $newItem;
                 }
             } else {
@@ -123,62 +114,18 @@ class ImportAmastyRates extends Command
         $stream->lock();
 
         /* Write Header */
-        $stream->writeCsv(['country', 'state', 'city', 'zip_from', 'zip_to', 'weight_from', 'weight_to', 'price_from', 'price_to', 'qty_from', 'qty_to', 'frpp', 'name_delivery', 'shipping_type', 'rate', 'ppp', 'frpuw', 'estimated_delivery']);
+        $stream->writeCsv(['method_id', 'country', 'state', 'city', 'zip_from', 'zip_to', 'weight_from', 'weight_to', 'price_from', 'price_to', 'qty_from', 'qty_to', 'frpp', 'name_delivery', 'shipping_type', 'rate', 'ppp', 'frpuw', 'estimated_delivery']);
         foreach ($data as $item) {
             $stream->writeCsv($item);
         }
-
 
         $content = [];
         $content['type'] = 'filename'; // must keep filename
         $content['value'] = $filepath;
         $content['rm'] = '0'; //remove csv from var folder
 
-        $csvfilename = 'Product.csv';
-        return $this->fileFactory->create($csvfilename, $content, DirectoryList::VAR_DIR);
-    }
+        $csvfilename = 'result_' . time() .  '.csv';
 
-    /**
-     * @param string $link
-     * @param int $type
-     * @return string|null
-     */
-    public function saveImage(string $link, int $type = 0)
-    {
-        try {
-            $fileName = basename($link);
-            $file = $this->file->read($link);
-            $pathToFile = $this->directoryList->getPath(DirectoryList::TMP) . '/' . $fileName;
-            if (!$this->file->write($pathToFile, $file)) {
-                var_dump($link);
-                die;
-            }
-            $_FILES['image'] = [
-                'name' => $fileName,
-                'tmp_name' => $pathToFile,
-                'type' => mime_content_type($pathToFile),
-                'size' => filesize($pathToFile),
-                'error' => ''
-            ];
-            switch ($type) {
-                case 0:
-                    $this->imageUploader->setBaseTmpPath('cms/tmp/page');
-                    $this->imageUploader->setBasePath('cms/page');
-                    $result = $this->imageUploader->saveFileToTmpDir('image');
-                    break;
-                case 1:
-                    $this->imageUploader->setBaseTmpPath('catalog/category');
-                    $this->imageUploader->setBasePath('catalog/category');
-                    $result = $this->imageUploader->saveFileToTmpDir('image');
-                    break;
-                default:
-                    die($link);
-            }
-            $this->file->rm($pathToFile);
-            return $result['name'];
-        } catch (Exception $e) {
-            echo $e->getMessage();
-            die;
-        }
+        return $this->fileFactory->create($csvfilename, $content, DirectoryList::VAR_DIR);
     }
 }
